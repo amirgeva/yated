63305088151829f1cd133cce76a7449ef7551db0
Amir Geva <amirgeva@gmail.com>
2017-09-23 18:15:55 +0300

    Padding utility

--- utils.py
+++ utils.py
@@ -11,3 +11,10 @@ def count_leading_spaces(s):
             break
         n+=1
     return n
+    
+def align(s,n):
+    if len(s)>n:
+        return s[0:n]
+    if len(s)<n:
+        return s+' '*(n-len(s))
+    return s
