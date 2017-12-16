7022dc1846c6124fc38a06cdff3d894618fd57d6
Amir Geva <amirgeva@gmail.com>
2017-12-16 23:21:25 +0200

    Fixed PEP8 formatting, and fixed color issue

--- utils.py
+++ utils.py
@@ -1,23 +1,23 @@
-
-
 class ExitException(Exception):
     pass
 
 
 def count_leading_spaces(s):
-    n=0
+    n = 0
     for c in s:
-        if c!=' ':
+        if c != ' ':
             break
-        n+=1
+        n += 1
     return n
-    
-def align(s,n):
-    if len(s)>n:
+
+
+def align(s, n):
+    if len(s) > n:
         return s[0:n]
-    if len(s)<n:
-        return s+' '*(n-len(s))
+    if len(s) < n:
+        return s + ' ' * (n - len(s))
     return s
-    
+
+
 def ctrl(key):
-    return ord(key)-ord('A')+1
+    return ord(key) - ord('A') + 1
